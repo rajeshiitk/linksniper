25ecbb994f289dc327467a0636d100a64ee0ce3a
Rajesh Choudhary <rajuvert18@gmail.com>
2023-12-19 00:02:48 +0530

    encryption middleware and methods on password

--- src/models/user.model.ts
+++ src/models/user.model.ts
@@ -1,9 +1,11 @@
 import { Schema, Document, model } from "mongoose";
+import bcryptjs from "bcryptjs";
 
 export interface IUser extends Document {
   name: string;
   email: string;
   password: string;
+  avatar?: string;
 }
 
 const userSchema = new Schema<IUser>(
@@ -24,11 +26,41 @@ const userSchema = new Schema<IUser>(
       required: [true, "password is required."],
       trim: true,
     },
+    avatar: {
+      type: String,
+      required: false,
+      default: "https://picsum.photos/200",
+    },
   },
   {
     timestamps: true,
   }
 );
+
+// middlewares
+userSchema.pre("save", async function (next) {
+  const user = this as IUser;
+  if (user.isModified("password")) {
+    // hash password
+    user.password = await bcryptjs.hash(user.password, 10);
+  }
+  next();
+});
+
+// methods
+
+// compare password
+userSchema.methods.ComparePassword = async function (password: string) {
+  const user = this as IUser;
+  return await bcryptjs.compare(password, user.password);
+};
+
+// update password
+userSchema.methods.UpdatePassword = async function (password: string) {
+  const user = this as IUser;
+  user.password = await bcryptjs.hash(password, 10);
+  await user.save();
+};
 
 const User = model<IUser>("User", userSchema);
 
